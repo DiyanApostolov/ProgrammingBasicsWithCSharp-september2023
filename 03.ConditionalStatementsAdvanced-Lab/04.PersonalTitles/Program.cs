dc5f8064d552219aa1110005c3f484509c1b71ed
Diyan Apostolov <diyan.apostolov@yahoo.com>
2023-09-16 16:41:16 +0300

    Add problems from Lab

---
+++ 03.ConditionalStatementsAdvanced-Lab/04.PersonalTitles/Program.cs
@@ -0,0 +1,27 @@
+﻿double age = double.Parse(Console.ReadLine());
+string gender = Console.ReadLine();
+
+if (gender == "f")
+{
+	if (age >= 16)
+	{
+		Console.WriteLine("Ms.");
+	}
+	else
+	{
+		Console.WriteLine("Miss");
+	}
+}
+else if (gender == "m")
+{
+	if (age >= 16)
+	{
+		Console.WriteLine("Mr.");
+	}
+	else
+	{
+		Console.WriteLine("Master");
+	}
+}
+
+
