c8a343a3af0b95bc7b374b8dfe865e4caeab144b
Diyan Apostolov <diyan.apostolov@yahoo.com>
2023-09-16 16:45:14 +0300

    Add solution for problem 11

---
+++ 03.ConditionalStatementsAdvanced-Lab/11.FruitShop/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _11.FruitShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //плод  banana  apple   orange  grapefruit kiwi pineapple grapes
+            //цена    2.50    1.20    0.85    1.45    2.70    5.50    3.85
+            //        2.70    1.25    0.90    1.60    3.00    5.60    4.20
+
+            string fruit = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Thursday" || dayOfWeek == "Wednesday" || dayOfWeek == "Friday")
+            {
+                switch (fruit)
+                {
+                    case "banana": price = 2.50; break;
+                    case "apple": price = 1.20; break;
+                    case "orange": price = 0.85; break;
+                    case "grapefruit": price = 1.45; break;
+                    case "kiwi": price = 2.70; break;
+                    case "pineapple": price = 5.50; break;
+                    case "grapes": price = 3.85; break;
+                    default: Console.WriteLine("error"); break;
+                }
+            }
+            else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+            {
+                switch (fruit)
+                {
+                    case "banana": price = 2.70; break;
+                    case "apple": price = 1.25; break;
+                    case "orange": price = 0.90; break;
+                    case "grapefruit": price = 1.60; break;
+                    case "kiwi": price = 3.00; break;
+                    case "pineapple": price = 5.60; break;
+                    case "grapes": price = 4.20; break;
+                    default: Console.WriteLine("error"); break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            if (price > 0)
+            {
+                double finalPrice = quantity * price;
+
+                Console.WriteLine($"{finalPrice:F2}");
+            }
+
+        }
+    }
+}
+
